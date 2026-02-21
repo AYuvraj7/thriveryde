2966c49cf071a064347dce76a272187920671201
AYuvraj7 <ayuvrajsingh90@gmail.com>
2026-02-21 18:51:37 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Root container missing in index.html");
-}
-
-createRoot(container).render(
-  <QueryClientProvider client={queryClient}>
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
+  <React.StrictMode>
     <App />
-  </QueryClientProvider>
+  </React.StrictMode>
 );
